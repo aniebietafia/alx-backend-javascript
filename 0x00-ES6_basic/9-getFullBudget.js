d38c4f9f8941e12434383565dc019ebb94f10db6
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-17 16:15:49 +0000

    using ES6 property syntax in an object

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,18 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+	const budget = getBudgetObject(income, gdp, capita);
+	const fullBudget = {
+		income,
+		gdp,
+		capita,
+		getIncomeInDollars: function (income) {
+			return `$${income}`;
+		},
+		getIncomeInEuros: function (income) {
+			return `${income} euros`;
+		},
+	};
+
+	return fullBudget;
+}
