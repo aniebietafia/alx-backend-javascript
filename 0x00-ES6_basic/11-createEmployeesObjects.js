b826470c1a4b7a165e8f39087c09dd29926a9e3d
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-17 22:52:08 +0000

    dynamic prperty in object

--- 0x00-ES6_basic/11-createEmployeesObjects.js
+++ 0x00-ES6_basic/11-createEmployeesObjects.js
@@ -1,3 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return { [departmentName]: [...employees] };
+  return { 
+	  [departmentName]: [
+		  ...employees
+	  ],
+  }
 }
