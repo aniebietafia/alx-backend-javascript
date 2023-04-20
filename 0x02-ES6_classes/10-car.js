5f81f5cb78212c52c3099a3943882f5ac5abe220
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-20 18:19:22 +0000

    creating a car class

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,16 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    // Create objs
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  // Methods
+
+  cloneCar() {
+    const NewObj = this.constructor[Symbol.species] || this.constructor;
+    const clone = new NewObj();
+    return clone;
+  }
+}
