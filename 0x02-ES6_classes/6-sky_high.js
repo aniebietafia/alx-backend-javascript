7efb8a2e8b8942a16839dc283b695a8ee1a2b4d2
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-20 18:11:54 +0000

    creting inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    // call constructor of super class (Building)
+    super(sqft);
+
+    // Instantiating Objects
+    this._floors = floors;
+  }
+
+  // Methods
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+  // Getters
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+}
