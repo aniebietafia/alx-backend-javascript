d16fe68f399a09d7ffd3607cfab71aa8191e7c44
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-20 18:14:00 +0000

    methods using custom key

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,12 @@
+export default class Airport {
+  constructor(name, code) {
+    //Instantiating Objects
+    this._code = code;
+    this._name = name;
+  }
+
+  // Methods
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
