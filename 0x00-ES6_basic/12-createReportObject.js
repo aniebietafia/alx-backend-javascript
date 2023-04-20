5ec5fe8fad8fb0efdb51d1c124b283385512f753
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-20 22:48:27 +0000

    create report object function

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments() {
-      return employeesList.length;
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
     },
   };
 }
