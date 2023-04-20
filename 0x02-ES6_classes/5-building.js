38aefcb23b4dd77ca41fd5d61beb8f3a935463b1
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-20 18:09:52 +0000

    class that extends from a method

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+export default class Building {
+  constructor(sqft) {
+    // Any class extending Building must implement below method
+    if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
+
+    // Create objs
+    this._sqft = sqft;
+  }
+  // Getters
+  get sqft() {
+    return this._sqft;
+  }
+}
