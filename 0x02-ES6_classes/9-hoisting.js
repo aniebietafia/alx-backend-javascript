407bf8ccafe106bad9734dee027190b017dfcc52
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-21 16:03:15 +0000

    Hoisting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -45,4 +45,3 @@ const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 const listOfStudents = [student1, student2, student3, student4, student5];
 
 export default listOfStudents;
-
