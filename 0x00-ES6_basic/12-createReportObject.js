463d65994b6e77d3edac8d65b796b23f4d30dc58
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-21 14:07:45 +0000

    Object that returns employees and number of departments

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,6 +2,11 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
     getNumberOfDepartments(employeesList) {
+      //   const departmentArray = [];
+      //   for (const department in employeesList) {
+      //     departmentArray.push(department);
+      //   }
+      //   return departmentArray.length;
       return Object.keys(employeesList).length;
     },
   };
