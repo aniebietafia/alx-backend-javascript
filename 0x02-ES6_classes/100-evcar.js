260d3bc0465227ebfb65551583190bb4e2326c8a
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-21 16:06:57 +0000

    implementing a car class

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,19 +1,14 @@
 import Car from './10-car';
 
-export default class EVCar extends Car {
+class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    // call constructor of super class (Building)
     super(brand, motor, color);
-
-    // Create objs
     this._range = range;
   }
 
-  // Methods
-
-  cloneCar() {
-    const NewObj = this.constructor[Symbol.species] || this.constructor;
-    const clone = new NewObj();
-    return clone;
+  static get [Symbol.species]() {
+    return Car;
   }
 }
+
+export default EVCar;
