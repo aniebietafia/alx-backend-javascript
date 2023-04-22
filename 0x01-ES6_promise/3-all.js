0b0f50e7adda09f0c1f856096fd65664732c0eda
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-22 20:05:22 +0000

    resolving all promises from a previous function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.allSettled([uploadPhoto(), createUser()])
