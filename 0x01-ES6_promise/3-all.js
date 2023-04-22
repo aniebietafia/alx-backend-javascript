de21b5996b449284d50b620b59d6f87decaea64f
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-22 20:18:10 +0000

    resolving all promises from a previous function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((data) => {
-      const { body } = data[0].value;
-      const { firstName } = data[1].value;
-      const { lastName } = data[1].value;
+      const { body } = data[0];
+      const { firstName } = data[1];
+      const { lastName } = data[1];
       console.log(`${body}, ${firstName}, ${lastName}`);
     })
     .catch(() => {
