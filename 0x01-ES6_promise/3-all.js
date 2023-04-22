bf1cce91ac9b581e8f973a09d3df665855a313ac
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-22 20:20:22 +0000

    resolving all promises from a previous function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup() {
       const { body } = data[0];
       const { firstName } = data[1];
       const { lastName } = data[1];
-      console.log(`${body}, ${firstName}, ${lastName}`);
+      console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
