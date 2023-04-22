ac4d886c7290d4c3353a1a777404947e8db894db
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-22 21:10:17 +0000

    settling all promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const userData = await signUpUser(firstName, lastName).then((data) => ({
-    status: 'fulfilled',
-    value: data,
-  }));
-
-  const fileData = await uploadPhoto(fileName).catch((err) => ({
-    status: 'rejected',
-    value: err.toString(),
-  }));
-
-  return Promise.resolve([userData, fileData]);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(), uploadPhoto()]).then((data) => {
+    return data;
+  });
 }
