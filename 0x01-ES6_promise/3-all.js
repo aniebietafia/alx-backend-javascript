450f77c545d12c008b2579301f1d2b3598ca85c2
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-22 19:59:05 +0000

    resolving all promises from a previous function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.allSettled([uploadPhoto(), createUser()])
     .then((data) => {
-      console.log(data[0].value['body'], data[1].value['firstName'], data[1].value['lastName']);
+      console.log(data[0].value.body, data[1].value.firstName, data[1].value.lastName);
     })
     .catch(() => {
       console.log('Signup system offline');
