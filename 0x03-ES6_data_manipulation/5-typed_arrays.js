a5abdfde62c671c5a0a3038b53d1e3b9f20987aa
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-25 13:24:50 +0000

    typed arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+export default function createInt8TypedArray(length, position, value) {
+  let arrayBuffer = new ArrayBuffer(length);
+  let view = new DataView(arrayBuffer);
+
+  if (position > length - 1) {
+    throw new Error('Position outside range');
+  }
+
+  view.setInt8(position, value);
+
+  return view;
+}
