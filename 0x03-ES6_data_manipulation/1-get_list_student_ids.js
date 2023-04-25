1beed812caffe3e68a11a47a8d1d30c953845155
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-25 07:24:59 +0000

    using map to retrieve student id

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,13 @@
 export default function getListStudentIds(objArray) {
+
   //checks if not an array
+
   if (!Array.isArray(objArray)) {
     return [];
   }
 
   // using map method to retrieve student id
+
   return objArray.map((student) => {
     return student.id;
   });
