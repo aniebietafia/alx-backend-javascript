a97d40867835ee2a30a00196915de3ecdb400266
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-25 12:16:05 +0000

    using maps to update unique items

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key] of map) {
+    if (map.get(key) === 1) map.set(key, 100);
+  }
+
+  return map;
+}
