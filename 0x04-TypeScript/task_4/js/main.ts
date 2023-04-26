a663f4a545987e99c395f0885bcb1afe0c95d018
AfiaAniebiet <aniebietafia87@gmail.com>
2023-04-26 17:36:06 +0000

    [Task 9 - 10] Namespace and declaration merging

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,30 @@
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Teacher.ts" />
+
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
+
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+  experienceTeachingC: 10,
+};
+
+console.log('C++');
+cpp.setTeacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher = cTeacher;
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher = cTeacher;
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
