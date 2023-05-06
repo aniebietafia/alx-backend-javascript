7432df5c16a98229226e65da1f070011190b4165
AfiaAniebiet <aniebietafia87@gmail.com>
2023-05-06 14:31:33 +0000

    iterator through object

--- 0x00-ES6_basic/101-iteratorThroughObject.js
+++ 0x00-ES6_basic/101-iteratorThroughObject.js
@@ -1,8 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employees = [];
-  for (const r of reportWithIterator) {
-    employees.push(r);
-  }
-
-  return employees.join(' | ');
+  return reportWithIterator.join(' | ');
 }
