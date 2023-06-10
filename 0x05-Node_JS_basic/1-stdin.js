85bdfba0b27f62b25b957af8d748515c60515f48
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-10 06:22:46 -0700

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -10,5 +10,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing \n');
 });
