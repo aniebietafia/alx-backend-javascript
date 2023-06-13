f0329874e0389c72a0feef1be7154dadd2c75a5d
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-13 02:36:04 -0700

    routes

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    AppController.getHomepage(req, res);
+  });
+
+  router.get('/students', (req, res) => {
+    StudentsController.getAllStudents(req, res, process.argv[2]);
+  });
+
+  router.get('/students/:major', (req, res) => {
+    StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
+  });
+
+export default router;
