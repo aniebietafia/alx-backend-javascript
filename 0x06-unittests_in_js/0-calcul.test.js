8c48d5815f5064a6358a6b83333be2fd22020522
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-14 10:45:20 -0700

    mocha test for task 0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('rounded', () => {
+  describe('#Math.round()', () => {
+    it('should round a and b and return the sum of it', () => {
+      assert.equal(Math.round(a) + Math.round(b), 'number');
+    });
+  });
+});
