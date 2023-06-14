1f377a0c23b55eed60056ca6d1bc7194429705c9
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-14 11:27:37 -0700

    Basic test with mocha

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,42 +1,22 @@
 const assert = require('assert');
-const mocha = require('mocha');
-
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-  it('should return sum of integers', () => {
+  it('checks the output', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, -1), 0);
-    assert.strictEqual(calculateNumber(1, -3), -2);
-  });
-
-  it('should round floats', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(0.1, 0), 0);
-    assert.strictEqual(calculateNumber(1.4, -4.5), -3);
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
+    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
   });
-
-  it('should return the rounded number if only one is provided', () => {
-    assert.strictEqual(calculateNumber(2), 2);
-    assert.strictEqual(calculateNumber(2.7), 3);
+  it('negative numbers', () => {
+    assert.strictEqual(calculateNumber(-1, 1), 0);
+    assert.strictEqual(calculateNumber(1, -1), 0);
+    assert.strictEqual(calculateNumber(-1, -1), -2);
   });
-
-  it('should cast non-numbers into numbers', () => {
-    assert.strictEqual(calculateNumber(true, '3'), 4);
-    assert.strictEqual(calculateNumber(1, '3.7'), 5);
-    assert.strictEqual(calculateNumber('1.2', 3.7), 5);
-  });
-
-  it('should throw typeerror if either param cannot be coerced to a number', () => {
-    assert.throws(() => calculateNumber('hello'), {
-      name: 'TypeError',
-      message: 'Parameters must be numbers'
-    });
-    assert.throws(() => calculateNumber(1.2, 'dog'), {
-      name: 'TypeError',
-      message: 'Parameters must be numbers'
-    });
+  it('checks arguments', () => {
+    assert.strictEqual(isNaN(calculateNumber(1)), true);
+    assert.strictEqual(isNaN(calculateNumber()), true);
   });
 });
