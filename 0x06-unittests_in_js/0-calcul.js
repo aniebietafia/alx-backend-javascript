10a2c30cc775e9dc239662622e99176361993ff2
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-14 11:27:00 -0700

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,4 @@
 function calculateNumber(a, b) {
-  if (Number.isNaN(a) || Number.isNaN(b)) {
-    throw new Error('Enter a number');
-  }
   return Math.round(Number(a)) + Math.round(Number(b));
 }
 
