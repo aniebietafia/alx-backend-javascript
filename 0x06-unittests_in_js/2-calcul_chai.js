8a3b57833c40ee9bf5f7c78387b3e6decbe8cbd0
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-15 04:28:56 -0700

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,16 +1,6 @@
 function calculateNumber(type, a, b) {
-  if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
-  }
-  if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
-  }
-  if (type === 'DIVIDE') {
-    if (Math.round(b) === 0) {
-      throw new Error('Error');
-    } else {
-      return Math.round(a) / Math.round(b);
-    }
-  }
+  if (type === 'SUM') return Math.round(a) + Math.round(b);
+  if (type === 'SUBTRACT') return Math.round(a) - Math.round(b);
+  if (type === 'DIVIDE') return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
 }
 module.exports = calculateNumber;
