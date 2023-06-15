a0f9f4abbab65951d8074bd41c0d2f545eb0cfad
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-15 06:57:00 -0700

    task 9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
-app.get('/', (req, res) => res.end('Welcome to the payment system'))
+app.get('/', (req, res) => {
+  res.end('Welcome to the payment system');
+});
+
 app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.send(`Payment methods for cart ${req.params.id}`)
-})
-app.listen(port, () => console.log(`API available on localhost port ${port}`));
+  res.end(`Payment methods for cart ${req.params.id}`);
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
