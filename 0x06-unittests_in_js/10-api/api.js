43d61ec0e5cca54ef8f8b8afd25c00fac4606ec5
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-15 06:52:00 -0700

    [Fix] Task 10

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,11 +1,18 @@
 const express = require('express');
+
 const app = express();
 const port = 7865;
 
-app.get('/', (req, res) => res.end('Welcome to the payment system'))
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.end('Welcome to the payment system');
+});
+
 app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`)
-})
+  res.end(`Payment methods for cart ${req.params.id}`);
+});
+
 app.get('/available_payments', (req, res) => {
   const obj = {
     payment_methods: {
@@ -15,7 +22,12 @@ app.get('/available_payments', (req, res) => {
   };
   res.json(obj);
 });
+
 app.post('/login', (req, res) => {
-  res.end(`Welcome ${req.body.userName}`);
+  const username = req.body.userName;
+  res.end(`Welcome ${username}`);
 });
-app.listen(port, () => console.log(`API available on localhost port ${port}`));
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
