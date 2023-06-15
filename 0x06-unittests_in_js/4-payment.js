ab124ee8f5fde0b42f848f86c4ecde46824b90e3
AfiaAniebiet <aniebietafia87@gmail.com>
2023-06-15 04:38:55 -0700

    stubs

---
+++ 0x06-unittests_in_js/4-payment.js
@@ -0,0 +1,7 @@
+const Utils = require('./utils.js');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${res}`);
+};
+module.exports = sendPaymentRequestToApi;
