c16ca6795eb1894bf9fa5d745ed62c29a100d2be
Aniebiet Afia <54312052+aniebietafia@users.noreply.github.com>
2023-08-22 18:15:28 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,27 +1,27 @@
-export default function createIteratorObject(report) {
-  let iterable = [];
-
-  if (!report.allEmployees || typeof report.allEmployees !== 'object') {
-    return iterable;
-  }
-
-  iterable = {
-    * [Symbol.iterator]() {
-      for (const value of Object.values(report.allEmployees)) {
-        for (const i of value) {
-          yield i;
-        }
-      }
-    },
-  };
-
-  return iterable;
-}
-
 // export default function createIteratorObject(report) {
-//   const result = [];
-//   for (const list of Object.values(report.allEmployees)) {
-//     result.push(...list);
+//   let iterable = [];
+
+//   if (!report.allEmployees || typeof report.allEmployees !== 'object') {
+//     return iterable;
 //   }
-//   return result;
+
+//   iterable = {
+//     * [Symbol.iterator]() {
+//       for (const value of Object.values(report.allEmployees)) {
+//         for (const i of value) {
+//           yield i;
+//         }
+//       }
+//     },
+//   };
+
+//   return iterable;
 // }
+
+export default function createIteratorObject(report) {
+  const result = [];
+  for (const list of Object.values(report.allEmployees)) {
+    result.push(...list);
+  }
+  return result;
+}
